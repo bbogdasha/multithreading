d2e2427cf3a0724c7a1d032a93d40f82ae8cd496
bbogdasha <bogdan4888@gmail.com>
2023-04-07 19:29:41 +0300

    Example with hackers and police

--- src/section1/thread_creation/part1/Main.java
+++ src/section1/thread_creation/part1/Main.java
@@ -1,32 +1,47 @@
+package section1.thread_creation.part1;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        Thread thread = new Thread(() -> {
+        //Implement the Runnable way
+        Thread thread1 = new Thread(() -> {
             System.out.println("We are now in thread " + Thread.currentThread().getName());
             System.out.println("Current thread priority is " + Thread.currentThread().getPriority());
         });
 
-        thread.setName("New worker Thread");
-        thread.setPriority(Thread.MAX_PRIORITY);
+        thread1.setName("New worker Thread");
+        thread1.setPriority(Thread.MAX_PRIORITY);
 
         System.out.println("We are in thread: " + Thread.currentThread().getName() + " before starting a new thread");
-        thread.start();
+        thread1.start();
         System.out.println("We are in thread: " + Thread.currentThread().getName() + " after starting a new thread");
 
-        
+
+        //Extends thread class way
+        Thread thread2 = new NewThread();
+        thread2.start();
+
+
         //Exception handler
-        Thread thread2 = new Thread(() -> {
+        Thread thread3 = new Thread(() -> {
             throw new RuntimeException("Intentional Exception");
         });
 
-        thread2.setName("Misbehaving thread");
+        thread3.setName("Misbehaving thread");
 
-        thread2.setUncaughtExceptionHandler((Thread t, Throwable e) -> {
+        thread3.setUncaughtExceptionHandler((Thread t, Throwable e) -> {
             System.out.println("A critical error happened in thread " + t.getName() +
                     " the error is " + e.getMessage());
         });
 
-        thread2.start();
+        thread3.start();
+    }
+
+    private static class NewThread extends Thread {
+        @Override
+        public void run() {
+            System.out.println("Hello from " + this.getName());
+        }
     }
 }
