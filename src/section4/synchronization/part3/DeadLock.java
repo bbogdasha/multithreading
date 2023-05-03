b0cf0672742e47766576931dad134ed0ed6332b0
bbogdasha <bogdan4888@gmail.com>
2023-05-03 18:20:05 +0300

    added deadlock method

--- src/section4/synchronization/part3/DeadLock.java
+++ src/section4/synchronization/part3/DeadLock.java
@@ -32,6 +32,27 @@ public class DeadLock {
         }
     }
 
+//    DeadLock situation
+    
+//    private static class DeadThreadTwo extends Thread {
+//
+//
+//        public void run() {
+//            synchronized (Lock2) {
+//                System.out.println("DeadThreadOne is holding LOCK 1...");
+//                try {
+//                    Thread.sleep(1000);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//                System.out.println("DeadThreadOne is waiting for Lock 2...");
+//                synchronized (Lock1) {
+//                    System.out.println("DeadThreadOne is holding Lock 1 and Lock 2...");
+//                }
+//            }
+//        }
+//    }
+
     private static class DeadThreadTwo extends Thread {
 
 
